0b10e5eccddcc30bd7d767264b4c3f39952ba968
Bogdan Sklyarov <n1ghtm6r9@gmail.com>
2024-03-07 21:18:12 +0200

    chore(): @nmxjs/types to peer dependencies

---
+++ types.d.ts
@@ -0,0 +1,13 @@
+declare module '@nmxjs/types' {
+  type FunctionType = any;
+  type DecoratorHandler = any;
+  type EntityDto = any;
+  enum StartModeEnum {
+    DEV = 'DEV',
+    WATCH = 'WATCH',
+    NATIVE = 'NATIVE',
+  }
+  enum EnvironmentEnum {
+    DEVELOPMENT = 'DEVELOPMENT',
+  }
+}
