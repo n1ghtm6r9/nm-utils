2afeb8e12d80488742614492d0c7362c0c649239
Bogdan Sklyarov <n1ghtm6r9@gmail.com>
2024-05-30 20:19:40 +0300

    fix(): bugs

--- src/TrimNumber/trimNumber.ts
+++ src/TrimNumber/trimNumber.ts
@@ -2,12 +2,12 @@ import math from 'big.js';
 import { numberToString } from '../NumberToString';
 import { ITrimNumberOptions } from './interfaces';
 
-export function trimNumber({ number, precision }: ITrimNumberOptions) {
+export function trimNumber({ number, precision }: ITrimNumberOptions): number {
   const strNumber = typeof number === 'string' ? number : numberToString(number);
   const dotIndex = strNumber.indexOf('.');
 
   if (dotIndex === -1) {
-    return number;
+    return typeof number === 'string' ? parseFloat(number) : number;
   }
 
   const nextIndexFromDot = math(dotIndex).add(1).toNumber();
